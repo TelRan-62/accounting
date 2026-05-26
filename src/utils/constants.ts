cb60befbb66a83038cd865a717269e4cd51db646
edward <edwardlfish@gmail.com>
2026-05-26 21:07:33 +0300

    Update `base_url` for relative paths and improve Nginx proxy configuration
    - Changed `base_url` to `/` for compatibility across environments.
    - Updated Nginx config to include `proxy_pass` for backend API routing.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,6 +2,6 @@ export const UPDATE_MODE_DEFAULT = 'UPDATE_MODE_DEFAULT';
 export const UPDATE_MODE_CHANGE_PASSWORD = 'UPDATE_MODE_CHANGE_PASSWORD';
 export const UPDATE_MODE_EDIT_PROFILE = 'UPDATE_MODE_EDIT_PROFILE';
 
-export const base_url = 'http://localhost:8080';
+export const base_url = '/';
 
 export const createToken = (userName: string, password: string) => 'Basic ' + btoa(`${userName}:${password}`)
